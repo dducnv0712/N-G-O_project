e248fa6d689880c2b28589940c788ab5cf5d10af
dduc_nv <ducbe2k2@gmail.com>
2021-08-11 13:57:29 +0700

    N-G-O

--- resources/views/components/ad_siderbar.blade.php
+++ resources/views/components/ad_siderbar.blade.php
@@ -1,56 +1,71 @@
 <nav id="sidebar" class="sidebar">
     <div class="sidebar-content js-simplebar">
-        <a class="sidebar-brand" href="index.html">
+        <a class="sidebar-brand" href="{{asset('/')}}">
             <span class="align-middle">AdminKit</span>
         </a>
 
         <ul class="sidebar-nav">
-
             <li class="sidebar-header">
-                Tools & Components
+                Pages
+            </li>
+
+            <li class="sidebar-item">
+                <a class="sidebar-link" href="{{asset('/')}}">
+                    <i class="align-middle" data-feather="sliders"></i> <span class="align-middle">Dashboard</span>
+                </a>
+            </li>
+
+            <li class="sidebar-item">
+                <a class="sidebar-link" href="{{asset('/admin/profile')}}">
+                    <i class="align-middle" data-feather="user"></i> <span class="align-middle">Profile</span>
+                </a>
             </li>
 
 
-               <li class="sidebar-item">
+
+            <li class="sidebar-header">
+                Website Manager
+            </li>
+            <li class="sidebar-item">
                 <a data-target="#ui" data-toggle="collapse" class="sidebar-link collapsed">
-                    <i class="align-middle" data-feather="briefcase"></i> <span class="align-middle">Contribute</span>
+                    <i class="align-middle" data-feather="edit"></i> <span class="align-middle">Posts</span>
                 </a>
                 <ul id="ui" class="sidebar-dropdown list-unstyled collapse " data-parent="#sidebar">
-                    <li class="sidebar-item"><a class="sidebar-link" href="{{url('/contributor')}}">Contributor</a></li>
-                    <li class="sidebar-item"><a class="sidebar-link" href="{{url('/credit-cart')}}">Credit cart</a></li>
-                    <li class="sidebar-item"><a class="sidebar-link" href="{{url('/digital-wallet')}}">Digital Wallet</a></li>
-                    <li class="sidebar-item"><a class="sidebar-link" href="{{url('/add')}}">Add Contribute</a></li>
-                    <li class="sidebar-item"><a class="sidebar-link" href="{{url('/list')}}">List Contribute</a></li>
-                    <li class="sidebar-item"><a class="sidebar-link" href="{{url('/update')}}">Update Contribute</a></li>
+                    <li class="sidebar-item"><a class="sidebar-link" href="ui-alerts.html">All Posts</a></li>
+                    <li class="sidebar-item"><a class="sidebar-link" href="ui-buttons.html">Form</a></li>
+                </ul>
+            </li>
+
+            <li class="sidebar-item">
+                <a data-target="#forms" data-toggle="collapse" class="sidebar-link collapsed">
+                    <i class="align-middle" data-feather="heart"></i> <span class="align-middle">Contributions</span>
+                </a>
+                <ul id="forms" class="sidebar-dropdown list-unstyled collapse " data-parent="#sidebar">
+                    <li class="sidebar-item"><a class="sidebar-link" href="forms-layouts.html">contribute</a></li>
+                    <li class="sidebar-item"><a class="sidebar-link" href="forms-basic-inputs.html">Debit Card</a></li>
+                    <li class="sidebar-item"><a class="sidebar-link" href="forms-basic-inputs.html">Paypal</a></li>
+
                 </ul>
             </li>
             <li class="sidebar-item">
-                <a data-target="#form" data-toggle="collapse" class="sidebar-link collapsed">
-                    <i class="align-middle" data-feather="briefcase"></i> <span class="align-middle">UI Elements</span>
+                <a data-target="#about" data-toggle="collapse" class="sidebar-link collapsed">
+                    <i class="align-middle" data-feather="check-circle"></i> <span class="align-middle">Pages Manager</span>
                 </a>
-                <ul id="form" class="sidebar-dropdown list-unstyled collapse " data-parent="#sidebar">
-                    <li class="sidebar-item"><a class="sidebar-link" href="ui-alerts.html">Alerts</a></li>
-                    <li class="sidebar-item"><a class="sidebar-link" href="ui-buttons.html">Buttons</a></li>
-                    <li class="sidebar-item"><a class="sidebar-link" href="ui-cards.html">Cards</a></li>
-                    <li class="sidebar-item"><a class="sidebar-link" href="ui-general.html">General</a></li>
-                    <li class="sidebar-item"><a class="sidebar-link" href="ui-grid.html">Grid</a></li>
-                    <li class="sidebar-item"><a class="sidebar-link" href="ui-modals.html">Modals</a></li>
-                    <li class="sidebar-item"><a class="sidebar-link" href="ui-typography.html">Typography</a></li>
+                <ul id="about" class="sidebar-dropdown list-unstyled collapse " data-parent="#sidebar">
+                    <li class="sidebar-item"><a class="sidebar-link" href="forms-layouts.html">About</a></li>
+                    <li class="sidebar-item"><a class="sidebar-link" href="forms-basic-inputs.html">Debit Card</a></li>
+                    <li class="sidebar-item"><a class="sidebar-link" href="forms-basic-inputs.html">Paypal</a></li>
+
                 </ul>
             </li>
 
-
-
         </ul>
 
-        <div class="sidebar-cta">
-            <div class="sidebar-cta-content">
-                <strong class="d-inline-block mb-2">Upgrade to Pro</strong>
-                <div class="mb-3 text-sm">
-                    Are you looking for more components? Check out our premium version.
-                </div>
-                <a href="https://adminkit.io/pricing" target="_blank" class="btn btn-primary btn-block">Upgrade to Pro</a>
-            </div>
-        </div>
+{{--        <div class="sidebar-cta">--}}
+{{--            <div class="sidebar-cta-content">--}}
+{{--                <strong class="d-inline-block mb-2">Upgrade to Pro</strong>--}}
+{{--                <a target="_blank" class="btn btn-primary btn-block">Account Management </a>--}}
+{{--            </div>--}}
+{{--        </div>--}}
     </div>
 </nav>

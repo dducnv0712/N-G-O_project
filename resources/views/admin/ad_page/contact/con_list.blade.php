6ad140737800744402ce6201da245b56daef2157
nguyenhongquang2002 <71063187+nguyenhongquang2002@users.noreply.github.com>
2021-08-17 23:54:45 +0700

    mo

--- resources/views/admin/ad_page/contact/con_list.blade.php
+++ resources/views/admin/ad_page/contact/con_list.blade.php
@@ -10,9 +10,6 @@
             <div class="col-auto ml-auto text-right mt-n1">
                 <nav aria-label="breadcrumb">
                     <ol class="breadcrumb bg-transparent p-0 mt-1 mb-0">
-                        <li class="breadcrumb-item"><a href="#">AdminKit</a></li>
-                        <li class="breadcrumb-item"><a href="#">Dashboards</a></li>
-                        <li class="breadcrumb-item active" aria-current="page">Analytics</li>
                     </ol>
                 </nav>
             </div>
@@ -40,45 +37,7 @@
                         <td>{{$item->messenger}}</td>
                         <td>{{$item->updated_at}}</td>
                         <td>{{$item->updated_at}}</td>
-{{--                        <td>--}}
-{{--                            @if($item->active == 1)--}}
-{{--                                <a href="{{url('admin/category/hidden',["id"=>$item->id])}}"><i class="align-middle text-success" data-feather="eye"></i></a>--}}
-{{--                            @else--}}
-{{--                                <a href="{{url('admin/category/appear',["id"=>$item->id])}}"><i class="align-middle text-danger" data-feather="eye-off"></i></a>--}}
-{{--                            @endif--}}
-{{--                        </td>--}}
-
                         <td class="table-action">
-                            <!-- Modal -->
-                            <div class="modal fade" id="Edit-{{$item->id}}" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                                <div class="modal-dialog modal-dialog-centered">
-                                    <div class="modal-content">
-                                        <form  action="{{url('admin/category/update',["id"=>$item->id])}}" method="post">
-                                            @csrf
-                                            <div class="modal-header">
-                                                <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
-                                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">X</button>
-                                            </div>
-                                            <div class="modal-body">
-                                                <div class="mb-3">
-                                                    <label for="nameCategory" class="form-label fw-bold">Tên Thể Loại</label>
-                                                    <input value="{{$item->name}}" id="nameCategory" name="name" class="form-control nameEdit rounded-3" required>
-                                                </div>
-                                            </div>
-                                            <div class="modal-footer">
-                                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                                <button type="submit" class="btn btn-primary">Submit</button>
-                                            </div>
-                                        </form>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <a data-bs-toggle="modal" data-bs-target="#Edit-{{$item->id}}"><i class="align-middle" data-feather="edit-2"></i></a>
-                            <a href="{{url('/admin/category/delete',["id"=>$item->id])}}"><i class="align-middle" data-feather="trash"></i></a>
-                        </td>
-                    </tr>
-
                 @endforeach
                 </tbody>
             </table>

05da5e9124681dfce1791c9fc8735c76ec4a11c6
dduc_nv <ducbe2k2@gmail.com>
2021-08-27 20:59:54 +0700

    N-G-O

--- resources/views/components/ad_nav.blade.php
+++ resources/views/components/ad_nav.blade.php
@@ -14,8 +14,20 @@
 
     <div class="navbar-collapse collapse">
         <ul class="navbar-nav navbar-align">
-
-
+            <li class="nav-item">
+                <div class="dropdown">
+                    <a class="dropdown-toggle nav-link" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                        <span class="flag-icon flag-icon-{{Config::get('languages')[App::getLocale()]['flag-icon']}}"></span> {{ Config::get('languages')[App::getLocale()]['display'] }}
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                        @foreach (Config::get('languages') as $lang => $language)
+                            @if ($lang != App::getLocale())
+                                <li><a class="dropdown-item" href="{{ route('lang.switch', $lang) }}"><span class="flag-icon flag-icon-{{$language['flag-icon']}}"></span> {{$language['display']}}</a></li>
+                            @endif
+                        @endforeach
+                    </ul>
+                </div>
+            </li>
             <li class="nav-item">
                 <a class="nav-icon dropdown-toggle d-inline-block d-sm-none" href="#" data-toggle="dropdown">
                     <i class="align-middle" data-feather="settings"></i>
